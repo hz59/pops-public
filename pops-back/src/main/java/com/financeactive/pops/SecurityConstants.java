939166ccc635ebd518fd5eea18fe48f81e4854cd
hz59 <78731414+hz59@users.noreply.github.com>
2021-07-09 01:15:15 +0200

    Update SecurityConstants.java

--- pops-back/src/main/java/com/financeactive/pops/SecurityConstants.java
+++ pops-back/src/main/java/com/financeactive/pops/SecurityConstants.java
@@ -1,9 +1,9 @@
 package com.financeactive.pops;
 
 public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String SECRET = ***************; // EDITED BY hz59 because it's confidential content.
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String TOKEN_PREFIX = **********; // EDITED BY hz59 because it's confidential content.
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users/sign-up";
 
